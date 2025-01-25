0ac0b9865db2fec27b2998ba01fb2a072a32c4f8
tlokotsi0 <amantusf@gmail.com>
2025-01-25 19:48:32 +0200

    Final work

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,8 +4,17 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // Create a Scripture object
-        Scripture scripture = new Scripture(new Reference("John", 3, 16), "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        //To exceed requirements, the program selects a random scripture from a list.
+        List<Scripture> scriptures = new List<Scripture>
+        {
+            new Scripture(new Reference("John", 3, 16), "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."),
+            new Scripture(new Reference("Romans", 8, 28), "And we know that all things work together for good to them that love God, to them that are the called according to his purpose."),
+            new Scripture(new Reference("Philippians", 4, 13), "I can do all things through Christ which strengtheneth me.") 
+        };
+
+        Random random = new Random();
+        int randomIndex = random.Next(0, scriptures.Count);
+        Scripture scripture = scriptures[randomIndex];
 
         while (!scripture.IsCompletelyHidden())
         {
@@ -19,7 +28,7 @@ public class Program
                 break;
             }
 
-            scripture.HideRandomWords(3); // Hide 3 random words at a time
+            scripture.HideRandomWords(3);
         }
 
         Console.Clear();
